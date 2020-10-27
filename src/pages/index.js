2099d8e9e1f9fbe3341c1f6c50b9ea9b4e582d00
Madhav Anand <madhav1004@icloud.com>
2020-10-27 16:21:12 +0530

    need to check on mobile browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,51 @@ export default function Home() {
   return (
     <div className={'home'}>
         <Header/>
-      I am amazon home page.
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1> <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        v
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+        <h1>I am amazon home page</h1>
+
+
     </div>
   )
 }
