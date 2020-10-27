dfec0d48ad2292ccb5c319c0a6ff3cfbc6efa6a9
Madhav Anand <madhav1004@icloud.com>
2020-10-27 16:27:38 +0530

    Revert "need to check on mobile browser"
    This reverts commit 2099d8e9e1f9fbe3341c1f6c50b9ea9b4e582d00.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,51 +5,7 @@ export default function Home() {
   return (
     <div className={'home'}>
         <Header/>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1> <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        v
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-        <h1>I am amazon home page</h1>
-
-
+      I am amazon home page.
     </div>
   )
 }
