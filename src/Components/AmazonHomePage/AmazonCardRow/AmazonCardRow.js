73970a2d3918fef3ff77e9b24cbc5b389076ac62
Madhav Anand <madhav1004@icloud.com>
2020-10-28 21:47:00 +0530

    Card Row is done just multiply them with props.

--- src/Components/AmazonHomePage/AmazonCardRow/AmazonCardRow.js
+++ src/Components/AmazonHomePage/AmazonCardRow/AmazonCardRow.js
@@ -28,11 +28,9 @@ function AmazonCardRow() {
                 <a href={'#'} className={productStyles.amazon_card_link}>Learn More</a>
             </AmazonCard>
             <AmazonCard>
-                <h4 className={productStyles.amazon_card_title}>Best Picks</h4>
+                <h4 className={productStyles.amazon_card_title}>Sign in for the best experience</h4>
 
-                <img className={productStyles.amazon_card_image} src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_Returns_1x._SY304_CB432774714_.jpg" alt="" />
-
-                <a href={'#'} className={productStyles.amazon_card_link}>Learn More</a>
+                <button className={productStyles.amazon_card_signin_button}>Sign in securely</button>
             </AmazonCard>
         </div>
     )
