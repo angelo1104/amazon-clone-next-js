883b13e429335138c7a1c64cc25021ad07f24a00
Madhav Anand <madhav1004@icloud.com>
2020-10-29 19:39:29 +0530

    scroll buttons are working though styles are not there.

--- src/Components/AmazonHomePage/AmazonProductsRow/AmazonProductsRow.js
+++ src/Components/AmazonHomePage/AmazonProductsRow/AmazonProductsRow.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './AmamzonProductsRow.module.css';
 import AmazonRowProduct from "./AmazonRowProduct/AmazonRowProduct";
 
 function AmazonProductsRow() {
+    const [translate,setTranslate] = useState(0)
+
+    const scrollRight = ()=>{
+        const container = document.querySelector(`.${styles.row_products_main}`)
+
+        container.scrollTo({
+            left: translate+200,
+            behavior: 'smooth'
+        })
+
+        setTranslate(translate+200)
+    }
+
     return(
         <div className={styles.amazon_products_row}>
             <div className={styles.amazon_row_header}>
@@ -17,6 +30,8 @@ function AmazonProductsRow() {
 
 
             <div className={styles.row_products_main}>
+                <button className={styles.row_products_scroll_button_left}>left</button>
+                <button onClick={scrollRight} className={styles.row_products_scroll_button_right}>right</button>
                 <AmazonRowProduct/>
                 <AmazonRowProduct/>
                 <AmazonRowProduct/>
