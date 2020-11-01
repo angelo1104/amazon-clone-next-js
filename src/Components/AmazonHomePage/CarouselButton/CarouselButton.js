7642a5a9991127ff0ef82c7ba01bff8e3dcb354d
Madhav Anand <madhav1004@icloud.com>
2020-11-01 20:15:26 +0530

    Carousel is there but not as I wanted need more control.

---
+++ src/Components/AmazonHomePage/CarouselButton/CarouselButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './CarouselButton.module.css';
+
+function CarouselButton() {
+    return (
+        <div className={styles.carousel_button}>
+            I am a button
+        </div>
+    )
+}
+
+export default CarouselButton;
