1f9f27c4d281e58fd2e0056d7e4402238ea72dd2
Madhav Anand <madhav1004@icloud.com>
2020-11-07 21:07:05 +0530

    Seller header is done.

--- src/Components/Seller/SellerHome/SellerHome.jsx
+++ src/Components/Seller/SellerHome/SellerHome.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from './SellerHome.module.css';
+import Header from "../../Header/Header";
+import SellerHeader from "./SellerHeader/SellerHeader";
 
 function SellerHome() {
     return(
         <div className={styles.seller_home}>
-            Seller home cook
+            <Header/>
+
+            <div>
+                <SellerHeader/>
+            </div>
         </div>
     )
 }
