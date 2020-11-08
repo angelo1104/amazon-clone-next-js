8fc5504d91deb0521532969e03830a653566f4f1
Madhav Anand <madhav1004@icloud.com>
2020-11-08 19:31:47 +0530

    Seller main third is done.

--- src/Components/Seller/SellerHome/SellerMain/SellerMain.js
+++ src/Components/Seller/SellerHome/SellerMain/SellerMain.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from './SellerMain.module.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import comma from './comma.png'
 
 function SellerMain() {
     return(
@@ -22,7 +24,9 @@ function SellerMain() {
                         <p className={styles.seller_fees}>$39.99 a month + selling fees</p>
                     </div>
                     <div className={styles.seller_main_first_right}>
-                        <img className={styles.seller_main_first_img} src="https://m.media-amazon.com/images/G/01/sell/images/prime-boxes/prime-boxes-2.png" alt=""/>
+                        <ScrollAnimation animateIn={'fadeIn'}>
+                            <img className={styles.seller_main_first_img} src="https://m.media-amazon.com/images/G/01/sell/images/prime-boxes/prime-boxes-2.png" alt=""/>
+                        </ScrollAnimation>
                     </div>
                 </div>
                 <svg alt="" className={styles.wave_svg} height="278px" preserveAspectRatio="none"
@@ -64,22 +68,64 @@ function SellerMain() {
                     <p className={styles.feature_card_desc}>
                         Leave the shipping, returns, and customer service to us with Fulfillment by Amazon (FBA). It’s the easiest way to reach 150 million paid Prime members around the world.                    </p>
 
-                    <a href="#" className={styles.seller_main_special_link}>How to get Started </a>
+                    <a href="#" className={styles.seller_main_special_link}>What is FBA? </a>
                 </div>
 
                 <div className={styles.seller_main_feature_card}>
-                    <img className={styles.feature_card_image} src="https://m.media-amazon.com/images/G/01/sp-marketing-toolkit/guides/design/iconography/Desktop_Computer._CB424651243_.svg" alt="" />
+                    <img className={styles.feature_card_image} src="https://m.media-amazon.com/images/G/01/sp-marketing-toolkit/guides/design/iconography/Paid_Services_USD._CB424651263_.svg" alt="" />
 
                     <h2 className={styles.feature_card_title}>
-                        Sell More
+                        Make money
                     </h2>
 
                     <p className={styles.feature_card_desc}>
-                        Fresh new startups and Fortune 500s. B2B and B2C. Brand owners and resellers. Independent third-party sellers sold more than a billion items during the 2019 holiday season alone.
-                    </p>
+                        In 2019, nearly 225,000 Amazon sellers worldwide eclipsed $100,000 in sales, up from nearly 200,000 in 2018—and 15,000 U.S. businesses broke $1 million.                    </p>
 
-                    <a href="#" className={styles.seller_main_special_link}>How to get Started </a>
+                    <a href="#" className={styles.seller_main_special_link}>Explore selling programs</a>
                 </div>
+            </div>
+
+            <div className={styles.seller_main_third}>
+                <div className={styles.seller_main_third_row}>
+                    <div className={styles.third_left}>
+                        <img className={styles.third_main_image} src="https://m.media-amazon.com/images/G/01/sell/images/Anker-01._CB1580163796_.jpg" alt=""/>
+                    </div>
+                    <div className={styles.third_right}>
+                        <img src={comma} alt=""/>
+
+                        <h3 className={styles.third_right_heading}>
+                            With all the infrastructure, systems, and processes Amazon built I realized that selling efficiency is no longer a problem.
+                        </h3>
+
+                        <div className={styles.name_author}>
+                            <img className={styles.name_author_image} src={'https://m.media-amazon.com/images/G/01/sell/images/headshots/syang-2-2x.png'} alt=""/>
+
+                            <div className={styles.name_author_about}>
+                                <h5 className={styles.name_author_name}>Steven Yang</h5>
+                                <p className={styles.name_author_desc}>Anker Technology</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.seller_main_third_links}>
+                    <h6 className={styles.seller_links_title}>learn more</h6>
+
+                    <div className={styles.seller_main_links_third}>
+                        <a href="#" className={styles.seller_main_special_link}>Sell online with Amazon: Beginner's Guide</a>
+                        <a href="#" className={styles.seller_main_special_link}>Grow your ecommerce business with Amazon</a>
+                        <a href="#" className={styles.seller_main_special_link}>Build an online store featuring your brand</a>
+                        <a href="#" className={styles.seller_main_special_link}>Watch free tutorial videos from Seller Academy</a>
+                        <a href="#" className={styles.seller_main_special_link}>Get help from an amazon account manager</a>
+                        <a href="#" className={styles.seller_main_special_link}>Amazon handmade, Amazon Business, and more</a>
+                        <a href="#" className={styles.seller_main_special_link}>How much does it cost to sell on amazon?</a>
+                        <a href="#" className={styles.seller_main_special_link}>Use FBA to scale your ecommerce business</a>
+                        <a href="#" className={styles.seller_main_special_link}>Advertise your products on Amazon</a>
+                        <a href="#" className={styles.seller_main_special_link}>Case studies about Amazon third-party sellers</a>
+                    </div>
+                </div>
+
+                <p className={styles.seller_third_individual}>Just have a few items to sell? <a href="#" className={styles.seller_main_special_link_inline}>Sign Up to become an individual seller</a></p>
             </div>
 
         </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import {StateProvider} from "../ContextApi/StateProvider";
 import reducer, {initialState} from "../ContextApi/reducers";
 import Head from "next/head";
 import AuthProvider from "../Components/Auth/AuthProvider/AuthProvider";
+import "animate.css/animate.min.css";
+
 
 function MyApp({ Component, pageProps }) {
 
@@ -11,6 +13,7 @@ function MyApp({ Component, pageProps }) {
       <StateProvider initialState={initialState} reducer={reducer}>
           <Head>
               <title>Create Next App</title>
+              <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet" />
               <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,500&family=Roboto:wght@300;400;500;700&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet" />
           </Head>
           <AuthProvider>
