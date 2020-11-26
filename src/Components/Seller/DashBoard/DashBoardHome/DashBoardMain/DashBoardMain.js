61b1c58ad21bc36dc6ca16d9484ce3ef344fb3f9
Madhav Anand <madhav1004@icloud.com>
2020-11-26 20:38:03 +0530

    Dashboard main is done for now. I mean products heading.

--- src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardMain.js
+++ src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardMain.js
@@ -1,11 +1,28 @@
 import React from "react";
 import styles from './DashBoardMain.module.css';
 import DashBoardMainCard from "./DashBoardMainCard/DashBoardMainCard";
+import DashBoardProductsCard from "./DashBoardProductsCard/DashBoardProductsCard";
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function DashBoardMain() {
     return(
         <div className={styles.main_dashboard}>
             <DashBoardMainCard/>
+
+
+            <div className={styles.products_ref}>
+                <h3 className={styles.products_ref_title}>Your Products</h3>
+
+                <div className={styles.search}>
+                    <div className={styles.search_box}>
+                        <SearchIcon className={styles.search_icon}/>
+                        <input placeholder={'Title, status, etc.'} className={styles.search_input} type="text" name="" id="" />
+                    </div>
+                    <button className={styles.search_button} type="submit">Search</button>
+                </div>
+            </div>
+            <DashBoardProductsCard/>
         </div>
     )
 }
---
+++ src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardProductsCard/DashBoardProductsCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './DashBoardProductsCard.module.css'
+
+function DashBoardProductsCard() {
+    return(
+        <div>
+            I am products card
+        </div>
+    )
+}
+
+export default DashBoardProductsCard
