dd5bea1e8d4f83fd26a600ebb0c93a6a4f96c13d
Madhav Anand <madhav1004@icloud.com>
2020-11-26 19:34:39 +0530

    DashBoard Product card is done

--- src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardMain.js
+++ src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardMain.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from './DashBoardMain.module.css';
+import DashBoardMainCard from "./DashBoardMainCard/DashBoardMainCard";
 
 function DashBoardMain() {
     return(
         <div className={styles.main_dashboard}>
-            Main Fame
+            <DashBoardMainCard/>
         </div>
     )
 }
