fcd2936c99ee18834cebe52cac8b3470528a8a1a
Madhav Anand <madhav1004@icloud.com>
2020-12-27 19:33:11 +0530

    Legals page is done still no legals in there.

--- src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardCreateProductContainer/DashBoardCreateProductContainer.js
+++ src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardCreateProductContainer/DashBoardCreateProductContainer.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import DashBoardCreateProduct from "../DashBoardCreateProduct/DashBoardCreateProduct";
 import DashBoardCreateProductImages from "../DashBoardCreateProductImages/DashBoardCreateProductImages";
+import DashBoardCreateProductLegals from "../DashBoardCreateProductLegals/DashBoardCreateProductLegals";
 
 function DashBoardCreateProductContainer() {
     const [page, setPage] = useState(1)
@@ -10,6 +11,8 @@ function DashBoardCreateProductContainer() {
             return <DashBoardCreateProduct setPage={setPage}/>
         case 2:
             return <DashBoardCreateProductImages setPage={setPage}/>
+        case 3:
+            return <DashBoardCreateProductLegals setPage={setPage}/>
         default:
             return <DashBoardCreateProduct setPage={setPage}/>
     }
---
+++ src/Components/Seller/DashBoard/DashBoardHome/DashBoardMain/DashBoardCreateProductLegals/DashBoardCreateProductLegals.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from './DashBoardCreateProductLegals.module.css'
+
+function DashBoardCreateProductLegals({setPage}) {
+
+    const moveBack = (event)=>{
+        event.preventDefault()
+
+        setPage(page => (page-1))
+    }
+
+    return(
+        <div className={styles.legals}>
+            <img className={styles.logo} src={'https://tinuiti.com/wp-content/uploads/legacysitecontent/cpcs/posts_01/2018/04/10095553/Amazon-seller-central.png'} alt=""/>
+
+            <form className={styles.legals_form}>
+                <h2 className={styles.title}>Additional Info?</h2>
+
+                <div className={styles.input_div}>
+                    <p className={styles.label}>Prices in USD</p>
+                    <input className={styles.input} type="number" autoComplete={false}/>
+                </div>
+
+                <div className={styles.input_div}>
+                    <p className={styles.label}>Search Term</p>
+                    <input className={styles.input} type="text" autoComplete={false}/>
+                </div>
+
+
+                <p className={styles.label_terms}>By creating an product on Amazon's seller central you acknowledge our terms and conditions. In order to get paid you should have your stripe account connected with us.</p>
+
+                <div className={styles.step_buttons_div}>
+                    <button className={styles.step_button} onClick={moveBack}>
+                        <span className={styles.step_button_arrow}>{'<'}</span> Prev
+                    </button>
+
+                    <button className={styles.step_button} >
+                        Create
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default DashBoardCreateProductLegals
