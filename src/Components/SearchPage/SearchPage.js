8e8153ae32ee143b74f828897ce1b90a0179af82
Angelo <madhav1004@icloud.com>
2021-01-08 18:47:50 +0530

    server side pagination done just need a spinner from lottiefiles.

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,34 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./SearchPage.module.css";
 import ProductTile from "./ProductTile/ProductTile";
+import VizSensor from "react-visibility-sensor";
+import productInstance from "../../axios/productInstance";
+import { useStateValue } from "../../ContextApi/StateProvider";
 
 function SearchPage({ serverHits }) {
   const [hits, setHits] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [next, setNext] = useState(false);
+  const [page, setPage] = useState(1);
 
-  const productLists = useRef();
+  const [{ searchText }] = useStateValue();
 
-  const handleScroll = (event) => {
-    console.log("JP");
+  const fetchProducts = async () => {
+    try {
+      console.log("fetched");
+
+      const { data } = await productInstance.post("/paginate", {
+        page: page + 1,
+        query: searchText,
+      });
+
+      setPage((pager) => pager + 1);
+      setNext(data.next);
+      setHits((hitler) => [...hitler, ...data.hits]);
+
+      setPage((page) => page + 1);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    setHits(serverHits.hits);
-    console.log(serverHits);
-  }, []);
+    console.log("Pagey", page, "Nexter", next);
+  }, [page, next]);
 
   useEffect(() => {
-    console.log("Hitty", hits);
-  }, [hits]);
+    if (visible) {
+      //loader is visible fetch products
+      if (next) {
+        //products are there you can fetch
+        fetchProducts();
+      } else {
+        //no more products
+      }
+    }
+  }, [visible]);
+
+  useEffect(() => {
+    setHits(serverHits.hits);
+    setNext(serverHits.next);
+  }, []);
 
   return (
     <div className={styles.products}>
       <div className={styles.ad}></div>
 
-      <div
-        className={styles.product_list}
-        ref={productLists}
-        onScroll={(event) => handleScroll(event)}
-      >
+      <div className={styles.product_list}>
         {hits?.map((hit, index) => {
           return (
             <ProductTile
@@ -51,6 +80,10 @@ function SearchPage({ serverHits }) {
             "https://m.media-amazon.com/images/I/71BMVOs2xML._AC_UY218_.jpg"
           }
         />
+
+        <VizSensor onChange={(isVisible) => setVisible(isVisible)}>
+          <div>Hi i am loader.</div>
+        </VizSensor>
       </div>
     </div>
   );
