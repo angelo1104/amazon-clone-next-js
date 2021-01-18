4e18fa0c130606ce99267cb2fbb915b9e0b745c3
Angelo <madhav1004@icloud.com>
2021-01-18 18:57:09 +0530

    login and sign up forms fixed. need to upgrade them.

--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,119 +1,141 @@
-import React, {useState} from "react";
-import styles from './Login.module.css';
+import React, { useState } from "react";
+import styles from "./Login.module.css";
 import Link from "next/link";
 import AuthFooter from "../AuthFooter/AuthFooter";
-import {auth} from "../../../firebase";
-import {useRouter} from 'next/router';
+import { auth } from "../../../firebase";
+import { useRouter } from "next/router";
 import Lottie from "lottie-react-web";
-import spinner from '../../../lottie/spinner.json'
+import spinner from "../../../lottie/ios-loader.json";
 
 function Login() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [processing,setProcessing] = useState(false);
-    const [error,setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [error, setError] = useState("");
 
-    const submitLogin = (event)=>{
-        event.preventDefault();
-        setError('')
+  const submitLogin = async (event) => {
+    event.preventDefault();
+    setError("");
 
-        if (email==='' || password===''){
-            setError('Please fill out all the fields')
-            setProcessing(false)
-        }else {
-            setProcessing(true)
-            auth().signInWithEmailAndPassword(email, password)
-                .then(authUser=>{
-                    router.push('/')
-                        .then(()=>{
-                            setProcessing(false)
-                        })
-                })
-                .catch(error=>{
-                    setError(error.message)
-                })
-        }
+    if (email === "" || password === "") {
+      setError("Please fill out all the fields");
+      setProcessing(false);
+    } else {
+      setProcessing(true);
+      try {
+        const user = await auth().signInWithEmailAndPassword(email, password);
 
+        await router.push("/");
+
+        setProcessing(false);
+      } catch (error) {
+        setProcessing(false);
+        setError(error.message);
+      }
     }
+  };
 
-    const goToSignUp = (event)=>{
-        event.preventDefault();
+  const goToSignUp = (event) => {
+    event.preventDefault();
 
-        router.push('/auth/email/sign-up');
+    router.push("/auth/email/sign-up");
+  };
+
+  const moveToNext = (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const inputs = Array.prototype.slice.call(
+        document.querySelectorAll("input")
+      );
+      const index =
+        (inputs.indexOf(document.activeElement) + 1) % inputs.length;
+      const input = inputs[index];
+      input.focus();
+      input.select();
     }
+  };
 
-    const moveToNext = (event)=>{
-        if (event.keyCode === 13) {
-            event.preventDefault()
-            const inputs =
-                Array.prototype.slice.call(document.querySelectorAll("input"))
-            const index =
-                (inputs.indexOf(document.activeElement) + 1) % inputs.length
-            const input = inputs[index]
-            input.focus()
-            input.select()
-        }
-    }
+  return (
+    <div className={styles.login}>
+      <Link href={"/"}>
+        <img
+          className={styles.amazon_logo}
+          src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png"
+          alt=""
+        />
+      </Link>
 
-    return(
-        <div className={styles.login}>
-            <Link href={'/'}>
-                <img className={styles.amazon_logo} src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png" alt="" />
-            </Link>
+      <form onSubmit={submitLogin} className={styles.login_form}>
+        <h1 className={styles.login_form_heading}>Sign-In</h1>
 
-            <form onSubmit={submitLogin} className={styles.login_form}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onKeyDown={moveToNext}
+          className={styles.login_input}
+          type="email"
+          placeholder={"email"}
+        />
 
-                <h1 className={styles.login_form_heading}>
-                    Sign-In
-                </h1>
+        <input
+          value={password}
+          placeholder={"password"}
+          style={{ background: processing && "#ff9900b0 !important" }}
+          onChange={(e) => setPassword(e.target.value)}
+          className={styles.login_input}
+          type="password"
+        />
 
-                <div className={styles.login_input_div}>
-                    <label className={styles.login_label} htmlFor={'#signup_email'} >Email</label>
-                    <input value={email} onChange={e=>setEmail(e.target.value)} onKeyDown={moveToNext} className={styles.login_input} type="email" name="" id={'signup_email'} />
-                </div>
+        <p className={styles.error}>{error}</p>
 
-                <div className={styles.login_input_div}>
-                    <label className={styles.login_label} htmlFor={'#signup_password'} >Password</label>
-                    <input value={password} onChange={e=>setPassword(e.target.value)} className={styles.login_input} type="password" name="" id={'signup_password'} />
-                </div>
-
-                <p className={styles.error}>{error}</p>
-
-                <button disabled={processing} className={styles.login_submit_button} type="submit">
-                    {
-                        !processing ? 'Continue' : <div className={styles.spinner}>
-                            <Lottie options={{
-                                animationData: spinner,
-                            }}/>
-                        </div>
-                    }
-                </button>
-
-                <p className={styles.login_terms}>By continuing over amazon clone you agree to the terms & conditions of our amazon clone.</p>
-
-
-                <div className={styles.need_help}>
-                    <Link href={'/auth/email/password-reset'}>
-                        <a className={styles.amazon_link}>
-                            Forgot Password?
-                        </a>
-                    </Link>
-                </div>
-            </form>
-
-            <div className={styles.divider}>
-                <h5 className={styles.divider_title}>New to Amazon?</h5>
+        <button
+          disabled={processing}
+          className={styles.login_submit_button}
+          type="submit"
+        >
+          {!processing ? (
+            "Continue"
+          ) : (
+            <div className={styles.spinner}>
+              <Lottie
+                options={{
+                  animationData: spinner,
+                }}
+              />
             </div>
+          )}
+        </button>
 
-            <button disabled={processing} onClick={goToSignUp} type={'submit'} className={styles.signup_button}>
-                Create your Amazon account
-            </button>
+        <p className={styles.login_terms}>
+          By continuing over amazon clone you agree to the terms & conditions of
+          our amazon clone.
+        </p>
 
-            <AuthFooter/>
+        <div className={styles.need_help}>
+          <Link href={"/auth/email/password-reset"}>
+            <a className={styles.amazon_link}>Forgot Password?</a>
+          </Link>
         </div>
-    )
+      </form>
+
+      <div className={styles.divider}>
+        <h5 className={styles.divider_title}>New to Amazon?</h5>
+      </div>
+
+      <button
+        disabled={processing}
+        onClick={goToSignUp}
+        type={"submit"}
+        className={styles.signup_button}
+      >
+        Create your Amazon account
+      </button>
+
+      <AuthFooter />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/Components/Auth/SignUp/SignUp.js
+++ src/Components/Auth/SignUp/SignUp.js
@@ -1,160 +1,173 @@
-import React, {useEffect, useState} from "react";
-import styles from './SignUp.module.css';
+import React, { useEffect, useState } from "react";
+import styles from "./SignUp.module.css";
 import Link from "next/link";
 import AuthFooter from "../AuthFooter/AuthFooter";
 import Lottie from "lottie-react-web";
-import spinner from '../../../lottie/spinner.json'
+import spinner from "../../../lottie/ios-loader.json";
 import authInstance from "../../../axios/authInstance";
-import {setDataUser} from "../../../ContextApi/actions";
-import {useStateValue} from "../../../ContextApi/StateProvider";
-import {auth} from "../../../firebase";
-import {useRouter} from "next/router";
+import { setDataUser } from "../../../ContextApi/actions";
+import { useStateValue } from "../../../ContextApi/StateProvider";
+import { auth } from "../../../firebase";
+import { useRouter } from "next/router";
 
 function SignUp() {
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [repassword, setRepassword] = useState('');
-    const [processing, setProcessing] = useState(false)
-    const [error, setError] = useState('')
-    const router = useRouter()
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [error, setError] = useState("");
+  const router = useRouter();
 
-    const [{user}, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
-    useEffect(()=>{
-       if (user) router.replace('/')
-    },[user])
+  useEffect(() => {
+    if (user) router.replace("/");
+  }, [user]);
 
-    const signup = (event) => {
-        event.preventDefault();
-        setError('')
-        setProcessing(true)
+  const signup = async (event) => {
+    event.preventDefault();
+    setError("");
+    setProcessing(true);
 
-        if (email === '' || name === '' || password === '' || repassword === '') {
-            setError('Please fill out all the fields')
-            setProcessing(false)
-        } else if (password !== repassword) {
-            setError('Passwords entered are not same.')
-            setProcessing(false)
-        } else {
-            setProcessing(true)
-            auth().createUserWithEmailAndPassword(email, password)
-                .then(authUser => {
-                    authUser.user.updateProfile({
-                        displayName: name,
-                    })
-                        .then(() => {
-                            authInstance.post('/new/user',{
-                                name: name,
-                                email: email,
-                                seller: false,
-                                uid: authUser.user.uid
-                            })
-                                .then((doc)=>{
-                                    dispatch(setDataUser(doc.data))
-                                    setProcessing(false)
-                                })
-                                .catch(error=>{
-                                    setProcessing(false);
-                                    setError(error.message)
-                                })
-                        })
-                })
-                .catch(error => {
-                    setProcessing(false)
-                    setError(error.message)
-                })
-        }
+    if (email === "" || name === "" || password === "" || repassword === "") {
+      setError("Please fill out all the fields");
+      setProcessing(false);
+    } else if (password !== repassword) {
+      setError("Passwords entered are not same.");
+      setProcessing(false);
+    } else {
+      setProcessing(true);
 
+      try {
+        const authUser = await auth().createUserWithEmailAndPassword(
+          email,
+          password
+        );
+
+        await authUser.user.updateProfile({
+          displayName: name,
+        });
+
+        const doc = await authInstance.post("/new/user", {
+          name: name,
+          email: email,
+          seller: false,
+          uid: authUser.user.uid,
+        });
+
+        dispatch(setDataUser(doc.data));
+        setProcessing(false);
+      } catch (error) {
+        setProcessing(false);
+        setError(error.message);
+      }
     }
+  };
 
-
-    const moveToNext = (event) => {
-        if (event.keyCode === 13) {
-            event.preventDefault()
-            const inputs =
-                Array.prototype.slice.call(document.querySelectorAll("input"))
-            const index =
-                (inputs.indexOf(document.activeElement) + 1) % inputs.length
-            const input = inputs[index]
-            input.focus()
-            input.select()
-        }
+  const moveToNext = (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      const inputs = Array.prototype.slice.call(
+        document.querySelectorAll("input")
+      );
+      const index =
+        (inputs.indexOf(document.activeElement) + 1) % inputs.length;
+      const input = inputs[index];
+      input.focus();
+      input.select();
     }
+  };
 
-    return (
-        <div className={styles.sign_up}>
-            <Link href={'/'}>
-                <img className={styles.amazon_logo}
-                     src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png" alt=""/>
+  return (
+    <div className={styles.sign_up}>
+      <Link href={"/"}>
+        <img
+          className={styles.amazon_logo}
+          src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png"
+          alt=""
+        />
+      </Link>
+
+      <form onSubmit={signup} className={styles.signup_form}>
+        <h1 className={styles.signup_form_heading}>Create account</h1>
+
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyDown={moveToNext}
+          className={styles.signup_input}
+          type="text"
+          placeholder={"name"}
+        />
+
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onKeyDown={moveToNext}
+          className={styles.signup_input}
+          type="email"
+          placeholder={"email"}
+        />
+
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onKeyDown={moveToNext}
+          className={styles.signup_input}
+          type="password"
+          placeholder={"password"}
+        />
+
+        <input
+          value={repassword}
+          onChange={(e) => setRepassword(e.target.value)}
+          className={styles.signup_input}
+          type="password"
+          placeholder={"re-enter password"}
+        />
+
+        <p className={styles.error}>{error}</p>
+
+        <button
+          disabled={processing}
+          style={{ background: processing && "#ff9900b0" }}
+          className={styles.signup_submit_button}
+          type="submit"
+        >
+          {!processing ? (
+            "Create your Amazon Account"
+          ) : (
+            <div className={styles.spinner}>
+              <Lottie
+                options={{
+                  animationData: spinner,
+                }}
+              />
+            </div>
+          )}
+        </button>
+
+        <p className={styles.signup_terms}>
+          By creating an account you agree to the terms & conditions of our
+          amazon clone.
+        </p>
+
+        <div className={styles.signup_form_already}>
+          <div className={styles.signup_gradient}></div>
+
+          <h5 className={styles.signup_already_p}>
+            Already have an account?
+            <Link href={"/auth/email/login"}>
+              <a className={styles.amazon_link_tm}>Sign in -></a>
             </Link>
-
-            <form onSubmit={signup} className={styles.signup_form}>
-                <h1 className={styles.signup_form_heading}>
-                    Create account
-                </h1>
-
-                <div className={styles.signup_input_div}>
-                    <label className={styles.signup_label} htmlFor={'#signup_name'}>Your name</label>
-                    <input value={name} onChange={e => setName(e.target.value)} onKeyDown={moveToNext}
-                           className={styles.signup_input} type="text" name="" id={'signup_name'}/>
-                </div>
-
-                <div className={styles.signup_input_div}>
-                    <label className={styles.signup_label} htmlFor={'#signup_email'}>Email</label>
-                    <input value={email} onChange={e => setEmail(e.target.value)} onKeyDown={moveToNext}
-                           className={styles.signup_input} type="email" name="" id={'signup_email'}/>
-                </div>
-
-                <div className={styles.signup_input_div}>
-                    <label className={styles.signup_label} htmlFor={'#signup_password'}>Password</label>
-                    <input value={password} onChange={e => setPassword(e.target.value)} onKeyDown={moveToNext}
-                           className={styles.signup_input} type="password" name="" id={'signup_password'}/>
-                </div>
-
-                <div className={styles.signup_input_div}>
-                    <label className={styles.signup_label} htmlFor={'#signup_reenter_pasword'}>Re-enter password</label>
-                    <input value={repassword} onChange={e => setRepassword(e.target.value)}
-                           className={styles.signup_input} type="password" name="" id={'signup_reenter_password'}/>
-                </div>
-
-                <p className={styles.error}>{error}</p>
-
-                <button disabled={processing} className={styles.signup_submit_button} type="submit">
-                    {
-                        !processing ? 'Create your Amazon Account' : <div className={styles.spinner}>
-                            <Lottie options={{
-                                animationData: spinner,
-                            }}/>
-                        </div>
-                    }
-
-                </button>
-
-                <p className={styles.signup_terms}>By creating an account you agree to the terms & conditions of our
-                    amazon clone.</p>
-
-                <div className={styles.signup_form_already}>
-
-                    <div className={styles.signup_gradient}>
-
-                    </div>
-
-                    <h5 className={styles.signup_already_p}>
-                        Already have an account?
-                        <Link href={'/auth/email/login'}>
-                            <a className={styles.amazon_link_tm}>
-                                Sign in ->
-                            </a>
-                        </Link>
-                    </h5>
-                </div>
-
-            </form>
-
-            <AuthFooter/>
+          </h5>
         </div>
-    )
+      </form>
+
+      <AuthFooter />
+    </div>
+  );
 }
 
 export default SignUp;
