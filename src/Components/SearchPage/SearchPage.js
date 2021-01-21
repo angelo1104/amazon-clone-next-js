5339f3ef8822b95dfa35ff4d3b82d2fd9dd437b9
Angelo <madhav1004@icloud.com>
2021-01-21 17:35:35 +0530

    aws amplify installed.

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -81,16 +81,16 @@ function SearchPage({ serverHits }) {
             );
           })}
 
-          <ProductTile
-            brand={"Apple Inc"}
-            title={
-              "Mac book pro 13 inches | Awesome stuff for you | Best hardware in the cupboard handpicked by IT professionals and a beautiful OS"
-            }
-            price={"1999.99"}
-            imageUrl={
-              "https://m.media-amazon.com/images/I/71BMVOs2xML._AC_UY218_.jpg"
-            }
-          />
+          {/*<ProductTile*/}
+          {/*  brand={"Apple Inc"}*/}
+          {/*  title={*/}
+          {/*    "Mac book pro 13 inches | Awesome stuff for you | Best hardware in the cupboard handpicked by IT professionals and a beautiful OS"*/}
+          {/*  }*/}
+          {/*  price={"1999.99"}*/}
+          {/*  imageUrl={*/}
+          {/*    "https://m.media-amazon.com/images/I/71BMVOs2xML._AC_UY218_.jpg"*/}
+          {/*  }*/}
+          {/*/>*/}
 
           <VizSensor onChange={(isVisible) => setVisible(isVisible)}>
             <div className={styles.lottie}>
