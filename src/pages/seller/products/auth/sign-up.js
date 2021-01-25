b3dda7d13965cb32c0201e5dcd8d9569ac8205d1
Madhav Anand <madhav1004@icloud.com>
2021-01-25 20:52:41 +0530

    fixed server side redirect in seller sign up.

--- src/pages/seller/products/auth/sign-up.js
+++ src/pages/seller/products/auth/sign-up.js
@@ -17,6 +17,22 @@ export default SellerSignUpPage;
 export async function getServerSideProps(ctx) {
   const { Auth } = withSSRContext(ctx);
 
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+
+    if (user?.attributes["custom:seller"] !== "false") {
+      console.log(user?.attributes["custom:seller"]);
+
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/seller/products/dashboard",
+        },
+        props: {},
+      };
+    }
+  } catch (error) {}
+
   return {
     props: {},
   };
